489c5950e306d443c0151e0893b7aeb347ce9e43
azmianshari <azmianshari123@gmail.com>
2024-12-15 11:22:16 +0700

    Memanggil beberapa fungsi dari MahasiswaDao

--- app/src/main/java/com/example/mvvmdengandatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/mvvmdengandatabase/repository/RepositoryMhs.kt
@@ -1,9 +1,18 @@
 package com.example.mvvmdengandatabase.repository
 
 import com.example.mvvmdengandatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 
 interface RepositoryMhs {
 
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<Mahasiswa>
+
+    fun getMhs(nim : String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
