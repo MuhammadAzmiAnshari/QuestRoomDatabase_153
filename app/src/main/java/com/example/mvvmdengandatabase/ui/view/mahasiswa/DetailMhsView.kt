433b9de07c1a476096cd1dfbfda0519d80d21815
Azmi <azmianshari123@gmail.com>
2024-12-17 01:40:11 +0700

    Memperbaiki beberapa error

--- app/src/main/java/com/example/mvvmdengandatabase/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/mvvmdengandatabase/ui/view/mahasiswa/DetailMhsView.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
