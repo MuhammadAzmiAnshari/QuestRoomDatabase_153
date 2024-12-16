8b6dc6a40a246a32a27a39ffaea9faa8c9aeef6d
Azmi <azmianshari123@gmail.com>
2024-12-16 23:30:48 +0700

    Melengkapi PenyediaViewModel.kt dengan mendaftarkan viewmodel yang sudah dibuat

--- app/src/main/java/com/example/mvvmdengandatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mvvmdengandatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.mvvmdengandatabase.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -14,6 +15,24 @@ object PenyediaViewModel{
             MahasiswaViewModel(
                 krsApp().containerApp.RepositoryMhs as LocalRepositoryMhs
             )
+        }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.RepositoryMhs
+            )
+        }
+        initializer {
+            DetailMahasiswaViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.RepositoryMhs
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.RepositoryMhs,
+
+                )
         }
     }
 }
